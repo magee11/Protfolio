80f34bd9f5dbad1d7517dc1cc2e367b359400e90
Magesh Azhakar <mageshma@amazon.com>
2024-06-29 16:40:34 +0530

    changes for resume and services

--- app/resume/page.jsx
+++ app/resume/page.jsx
@@ -1,9 +1,260 @@
-import React from 'react'
+"use client";
+import batch from "../../public/images/resume/badge.svg";
+import cap from "../../public/images/resume/cap.svg";
+import {
+  FaHtml5,
+  FaCss3,
+  FaReact,
+  FaNodeJs,
+  FaFigma,
+  FaJs,
+  FaGit,
+  FaDocker,
+  FaAws,
+} from "react-icons/fa";
+import { SiTailwindcss, SiNextdotjs, SiAdobexd } from "react-icons/si";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
+import { ScrollArea } from "@/components/ui/scroll-area";
+import { motion } from "framer-motion";
+import {
+  Tooltip,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@radix-ui/react-tooltip";
+import { TooltipContent } from "@/components/ui/tooltip";
+
+const about = {
+  title: "",
+  description:
+    "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deleniti odio illo dolore labore eligendi debitis quos voluptate excepturi ex magnam. Omnis non odio aperiam corrupti exercitationem! Dignissimos, optio? Cupiditate, exercitationem!",
+  info: [
+    { fieldName: "Name", fieldValue: "Magesh" },
+    { fieldName: "Stack", fieldValue: "Frontend Developer" },
+    { fieldName: "Email", fieldValue: "mageshmarch@gmail.com" },
+    { fieldName: "Phone", fieldValue: "+91 9345236199" },
+    { fieldName: "Exprience", fieldValue: "1+ years" },
+    { fieldName: "Nationality", fieldValue: "Indian" },
+    { fieldName: "Languages", fieldValue: "English ,Tamil" },
+  ],
+};
+
+const exprience = {
+  icon: batch,
+  title: "My Exprience",
+  description:
+    "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloremque possimus voluptas architecto molestiae reprehenderit omnis, error hic fuga inventore dicta officiis similique aut aliquam animi esse nostrum quaerat ipsum id!",
+  item: [
+    {
+      company: "Divum Corporate Sevices Private Limited",
+      position: "Frontend Developer",
+      duration: "2023 - Present",
+      location: "Bengaluru",
+    },
+    {
+      company: "Divum Corporate Sevices Private Limited",
+      position: "Frontend Developer",
+      duration: "2023 - Present",
+      location: "Bengaluru",
+    },
+  ],
+};
+
+const education = {
+  icon: cap,
+  title: "My Education",
+  description:
+    "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloremque possimus voluptas architecto molestiae reprehenderit omnis, error hic fuga inventore dicta officiis similique aut aliquam animi esse nostrum quaerat ipsum id!",
+  item: [
+    {
+      institution: "E.G.S Pillay Engineering College",
+      degree: "BE - Computer Science and Engineering",
+      duration: "2019 - 2023",
+    },
+  ],
+};
+
+const skills = {
+  title: "My Skills",
+  description:
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia, perferendis ipsum soluta tempore nesciunt vitae unde harum nostrum quis. Sint nemo officia fuga. Ipsum aliquid impedit adipisci perspiciatis eveniet exercitationem.",
+  skillList: [
+    {
+      icon: <FaHtml5 />,
+      name: "HTML 5",
+    },
+    {
+      icon: <FaCss3 />,
+      name: "CSS 3",
+    },
+    {
+      icon: <FaReact />,
+      name: "React JS",
+    },
+    {
+      icon: <FaNodeJs />,
+      name: "Node JS",
+    },
+    {
+      icon: <FaFigma />,
+      name: "Figma",
+    },
+    {
+      icon: <FaJs />,
+      name: "Java Script",
+    },
+    {
+      icon: <FaGit />,
+      name: "Git",
+    },
+    {
+      icon: <FaDocker />,
+      name: "Docker",
+    },
+    {
+      icon: <FaAws />,
+      name: "Amazon Web Services",
+    },
+    {
+      icon: <SiTailwindcss />,
+      name: "Tailwind CSS",
+    },
+    {
+      icon: <SiNextdotjs />,
+      name: "Next JS",
+    },
+    {
+      icon: <SiAdobexd />,
+      name: "Adobe XD",
+    },
+  ],
+};
 const Resume = () => {
   return (
-    <div>Resume</div>
-  )
-}
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{
+        opacity: 1,
+        transition: {
+          delay: 1.4,
+          duration: 0.4,
+          ease: "easeIn",
+        },
+      }}
+      className="min-h-[80vh] flex items-center justify-center py-12 xl:py-0"
+    >
+      <div className="container mx-auto">
+        <Tabs
+          defaultValue="exprience"
+          className="flex flex-col xl:flex-row gap-[60px]"
+        >
+          <TabsList className="flex flex-col w-full max-w-[380px] mx-auto xl:mx-0 gap-6">
+            <TabsTrigger value="exprience">Exprience</TabsTrigger>
+            <TabsTrigger value="education">Education</TabsTrigger>
+            <TabsTrigger value="skills">Skills</TabsTrigger>
+            <TabsTrigger value="about">About Me</TabsTrigger>
+          </TabsList>
+          <div className="min-h-[70vh] w-full">
+            <TabsContent value="exprience" className="w-full">
+              <div className=" flex flex-col gap-[30px] text-center xl:text-left">
+                <h3 className="text-4xl font-bold">{exprience.title}</h3>
+                <p className="max-w-[600px] text-white/60 mx-auto xl:mx-0">
+                  {exprience.description}
+                </p>
+                <ScrollArea className="h-[400px]">
+                  <ul className="grid grid-cols lg:grid-cols-2 gap-[30px]">
+                    {exprience.item.map((item, index) => (
+                      <li className="bg-[#232329] h-[184px] py-6 px-10 rounded-xl flex flex-col items-center justify-center lg:items-start gap-1">
+                        <span className="text-accent">{item.duration}</span>
+                        <h3 className="text-xl max-w-[260px] min-h-[60px] text-center lg:text-left">
+                          {item.position}
+                        </h3>
+                        <div className="flex items-center gap-3">
+                          <span className="w-[6px] h-[6px] rounded-full bg-accent"></span>
+                          <p className="text-white/60">{item.company}</p>
+                        </div>
+                      </li>
+                    ))}
+                  </ul>
+                </ScrollArea>
+              </div>
+            </TabsContent>
+            <TabsContent value="education" className="w-full">
+              <div className=" flex flex-col gap-[30px] text-center xl:text-left">
+                <h3 className="text-4xl font-bold">{education.title}</h3>
+                <p className="max-w-[600px] text-white/60 mx-auto xl:mx-0">
+                  {education.description}
+                </p>
+                <ScrollArea className="h-[400px]">
+                  <ul className="grid grid-cols lg:grid-cols-2 gap-[30px]">
+                    {education.item.map((item, index) => (
+                      <li className="bg-[#232329] h-[184px] py-6 px-10 rounded-xl flex flex-col items-center justify-center lg:items-start gap-1">
+                        <span className="text-accent">{item.duration}</span>
+                        <h3 className="text-xl max-w-[260px] min-h-[60px] text-center lg:text-sm lg:text-left">
+                          {item.degree}
+                        </h3>
+                        <div className="flex items-center gap-3">
+                          <span className="w-[6px] h-[6px] rounded-full bg-accent"></span>
+                          <p className="text-white/60">{item.institution}</p>
+                        </div>
+                      </li>
+                    ))}
+                  </ul>
+                </ScrollArea>
+              </div>
+            </TabsContent>
+            <TabsContent value="skills" className="w-full">
+              <div className="flex flex-col gap-[30px]">
+                <div className="flex flex-col gap-[30px] text-center xl:text-left">
+                  <h3 className="text-4xl font-extrabold">{skills.title}</h3>
+                  <p className="max-w-[600px] text-white/60 mx-auto xl:mx-0">
+                    {skills.description}
+                  </p>
+                </div>
+                <ul className="grid grid-cols-2 sm:gird-cols-3 md:grid-cols-4 xl:gap-[30px]">
+                  {skills.skillList.map((item, index) => (
+                    <li key={index}>
+                      <TooltipProvider>
+                        <Tooltip>
+                          <TooltipTrigger className="w-full h-[120px] bg-[#232329] rounded-lg flex justify-center items-center group">
+                            <div className="text-6xl group-hover:text-accent transition-all duration-300">
+                              {item.icon}
+                            </div>
+                          </TooltipTrigger>
+                          <TooltipContent>
+                            <p>{item.name}</p>
+                          </TooltipContent>
+                        </Tooltip>
+                      </TooltipProvider>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            </TabsContent>
+            <TabsContent
+              value="about"
+              className="w-full text-center xl:text-left"
+            >
+              <div className="flex flex-col gap-[20px]">
+                <h3 className="text-4xl font-extrabold">{about.title}</h3>
+                <p className="max-w-[600px] text-white/60 mx-auto xl:mx-0">
+                  {about.description}
+                </p>
+                {
+                  about.info.map((item,index)=>(
+                    <li key={index} className="flex items-center justify-center xl:justify-start gap-4">
+                      <p>{item.fieldName}</p>
+                      <p className="text-white/60">{item.fieldValue}</p>
+                    </li>
+                  ))
+                }
+              </div>
+            </TabsContent>
+          </div>
+        </Tabs>
+      </div>
+    </motion.div>
+  );
+};
 
-export default Resume
+export default Resume;
--- app/service/page.jsx
+++ app/service/page.jsx
@@ -1,9 +1,75 @@
-import React from 'react'
+"use client";
+import { BsArrowDownRight } from "react-icons/bs";
+import Link from "next/link";
+import { motion } from "framer-motion";
+
+const service = [
+  {
+    num: "01",
+    title: "React JS Developer",
+    desceription:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil illo, assumenda vero voluptas facilis modi veniam natus sunt ratione neque esse quia quo corrupti temporibus soluta molestias vel ad eaque.",
+    href: "",
+  },
+  {
+    num: "02",
+    title: "Backend Developer",
+    desceription:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil illo, assumenda vero voluptas facilis modi veniam natus sunt ratione neque esse quia quo corrupti temporibus soluta molestias vel ad eaque.",
+    href: "",
+  },
+  {
+    num: "03",
+    title: "MERN Stack Developer",
+    desceription:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil illo, assumenda vero voluptas facilis modi veniam natus sunt ratione neque esse quia quo corrupti temporibus soluta molestias vel ad eaque.",
+    href: "",
+  },
+  {
+    num: "04",
+    title: "SEO",
+    desceription:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil illo, assumenda vero voluptas facilis modi veniam natus sunt ratione neque esse quia quo corrupti temporibus soluta molestias vel ad eaque.",
+    href: "",
+  },
+];
 
 const Service = () => {
   return (
-    <div>Service</div>
-  )
-}
+    <section className="min-h-[80vh] flex flex-col justify-center py-12 xl:py-0">
+      <div className=" container mx-auto">
+        <motion.div
+          initial={{ opacity: 0 }}
+          className="grid grid-cols-1 md:grid-cols-2 gap-[60px]"
+          animate={{
+            opacity: 1,
+            transition: {
+              delay: 1.4,
+              duration: 0.4,
+              ease: "easeIn",
+            },
+          }}
+        >
+          {service.map((service, index) => {
+            return (
+              <div key={index} className="flex-1 flex flex-col justify-center gap-6 group">
+                <div className="w-full flex  justify-between items-center">
+                  <div className="text-5xl font-extrabold group-hover:text-accent transition-all duration-500">{service.num}</div>
+                  <Link href={service.href} className="w-[50px] h-[50px] rounded-full bg-white text-black group-hover transition-all duration-500 flex justify-center items-center hover:-rotate-45">
+                    <BsArrowDownRight />
+                  </Link>
+                </div>
+                {/* title  */}
+                  <h2 className=" text-[32px] text-white group-hover:text-accent transition-all duration-500 font-bold leading-none">{service.title}</h2>
+                  <p className="text-white/70 group-hover:text-white transition-all duration-500">{service.desceription}</p>
+                  <div className="border-b border-white/20 w-full"></div>
+              </div>
+            );
+          })}
+        </motion.div>
+      </div>
+    </section>
+  );
+};
 
-export default Service
+export default Service;
